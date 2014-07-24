defffc1911d9b809ba8e6bdaa5ced82c692e7900
Richeve S. Bebedor <volkovasystems@gmail.com>
2014-07-24 19:58:41 +0800

    Added new strings for the puppetry.

--- string-set.js
+++ string-set.js
@@ -3,7 +3,31 @@ var stringSet = {
 		"http://webnified-volkovasystems.rhcloud.com/"
 	],
 
-	"/data/image/logo.png": [
+	"/data/image/world/dominator": [
 		"https://dl.dropboxusercontent.com/u/10676965/batman.jpg"
+	],
+
+	"/data/company/webnified/tagline": [
+		"https://dl.dropboxusercontent.com/u/10676965/webnified-tagline.json"
+	],
+
+	"/data/company/webnified/about-us": [
+		"https://dl.dropboxusercontent.com/u/10676965/webnified-about-us.json"
+	],
+
+	"/data/company/webnified/about-us/long": [
+		"https://dl.dropboxusercontent.com/u/10676965/webnified-about-us-long.json"
+	],
+
+	"/data/company/webnified/image/logo/webnified-logo-revised.jpg": [
+		"https://dl.dropboxusercontent.com/u/10676965/webnified-logo-revised.jpg"
+	],
+
+	"/data/company/webnified/image/logo/webnified-logo-horizontal.jpg": [
+		"https://dl.dropboxusercontent.com/u/10676965/webnified-logo-horizontal.jpg"
+	],
+
+	"/data/company/webnified/image/logo/webnified-logo.jpg": [
+		"https://dl.dropboxusercontent.com/u/10676965/webnified-logo.jpg"
 	]
 };
